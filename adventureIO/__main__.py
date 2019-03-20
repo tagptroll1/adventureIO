2d219a7e0cc5ff5f80a44677566e2ad987956542
Tagptroll1 <thomas@galehuset.org>
2019-03-20 22:18:10 +0100

    result of git pull?

--- adventureIO/__main__.py
+++ adventureIO/__main__.py
@@ -11,21 +11,26 @@ OWNERS = (IDS.creator, IDS.benny)
 log = logging.getLogger(__name__)
 bot = AdventureBot(command_prefix=BotConfig.prefix)
 
+
 @bot.group(name="git")
 async def git_group(ctx):
     """Group for git commands"""
+
 
 @git_group.command(name="pull")
 async def git_pull_command(ctx):
     """Pulls any updates for the bot from git"""
     try:
-        os.system("git pull origin master")
+        ans = os.system("git pull origin master")
+        await ctx.send(ans)
     except Exception as e:
         await ctx.send(f"```{e}```")
+
 
 @bot.group(name="bot")
 async def bot_group(ctx):
     """Group for bot commands"""
+
 
 @bot_group.command(name="shutdown", aliases=("exit", "kill"))
 async def bot_shutdown_command(ctx):
@@ -33,9 +38,11 @@ async def bot_shutdown_command(ctx):
         return
     await bot.logout()
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong <@234048816033038337>")
+
 
 @bot_group.command()
 async def reload_cogs(ctx):
