21fb6a7de0e62d5845665e373b08c048293b2e4c
Tagptroll1 <thomas@galehuset.org>
2019-03-20 20:29:09 +0100

    removed os

--- adventureIO/__main__.py
+++ adventureIO/__main__.py
@@ -31,12 +31,6 @@ async def bot_shutdown_command(ctx):
     if ctx.author.id not in OWNERS:
         return
     await bot.logout()
-    
-@bot_group.command(name="os")
-async def bot_os_command(ctx, *, text):
-    if ctx.author.id not in OWNERS:
-        return
-    os.system(text)
 
 @bot.command()
 async def ping(ctx):
